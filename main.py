6fdbe169a5dfbdbd210e95a704d33a9e1bdc91e9
Matteo Cacciola <matteo.cacciola@gmail.com>
2026-01-18 21:25:11 +0100

    updating the entire structure of the hybrid collections and methods referring to them

--- main.py
+++ main.py
@@ -3,80 +3,37 @@ import time
 from langchain_core.documents import Document as LangChainDocument
 
 from cat import hook, StrayCat, log, UserMessage, AgenticWorkflowOutput, RecallSettings
-from cat.services.memory.models import Document, PointStruct
+from cat.services.memory.models import Document, PointStruct, DocumentRecall, ScoredPoint
 from cat.utils import run_sync_or_async
 
 # global variables
-hybrid_collection_name = "declarative_hybrid"
+hybrid_collection_names = ["declarative_hybrid", "episodic_hybrid"]
 k = 5
 k_prefetched = 10
 threshold = 0.5
 
 
-@hook(priority=99)
-def before_cat_reads_message(user_message: UserMessage, cat) -> UserMessage:
-    global k, threshold, k_prefetched
-    settings = cat.mad_hatter.get_plugin().load_settings()
-    k = settings["number_of_hybrid_items"]
-    k_prefetched = settings["number_of_prefetched_items"]
-    threshold = settings["hybrid_threshold"]
-    return user_message
+async def delete_hybrid_collection_if_exists(collection_name: str, cat: StrayCat):
+    await cat.vector_memory_handler.delete_collection(collection_name=collection_name)
+    log.info("Hybrid collection deleted")
 
 
-@hook(priority=99)
-def agent_fast_reply(cat: StrayCat) -> AgenticWorkflowOutput | None:
-    global hybrid_collection_name
-
-    user_message: str = cat.working_memory.user_message.text
-    if not user_message.startswith("@hybrid"):
-        return None
-
-    if user_message == "@hybrid init":
-        run_sync_or_async(delete_hybrid_collection_if_exists, cat, hybrid_collection_name)
-        run_sync_or_async(create_hybrid_collection_if_not_exists, cat, hybrid_collection_name)
-
-        return AgenticWorkflowOutput(output="Hybrid collection initialized.")
-    if user_message == "@hybrid migrate":
-        points = run_sync_or_async(get_declarative_points, cat)
-        run_sync_or_async(populate_hybrid_collection, points, cat)
-
-        # add 5-second wait time to ensure data is committed
-        time.sleep(5)
-
-        return AgenticWorkflowOutput(output="Hybrid collection populted.")
-
-    return None
-
-
-async def get_declarative_points(cat):
-    return await cat.vector_memory_handler.get_all_points(collection_name="declarative", with_vectors=True)
-
-
-# hybrid collection management
-@hook
-def after_cat_bootstrap(cat: StrayCat):
-    run_sync_or_async(create_hybrid_collection_if_not_exists, cat, hybrid_collection_name)
-
-
-async def create_hybrid_collection_if_not_exists(cat, collection_name):
+async def create_hybrid_collection_if_not_exists(collection_name: str, cat: StrayCat):
     dense_vector_name = "dense"
     sparse_vector_name = "sparse"
     await cat.vector_memory_handler.create_hybrid_collection(collection_name, dense_vector_name, sparse_vector_name)
     log.info("Hybrid collection created")
 
 
-async def delete_hybrid_collection_if_exists(cat, collection_name):
-    await cat.vector_memory_handler.delete_collection(collection_name=collection_name)
-    log.info("Hybrid collection deleted")
+async def get_memory_points(hybrid_collection_name: str, cat: StrayCat) -> List[PointStruct]:
+    points, _ = await cat.vector_memory_handler.get_all_tenant_points(
+        collection_name=hybrid_collection_name.replace("_hybrid", ""), with_vectors=True
+    )
+    return [PointStruct(id=p.id, vector=p.vector, payload=p.payload) for p in points]
 
 
-@hook
-def after_rabbithole_stored_documents(source: str, stored_points: List[PointStruct], cat):
-    run_sync_or_async(populate_hybrid_collection, stored_points, cat)
-
-
-async def populate_hybrid_collection(stored_points, cat):
-    global hybrid_collection_name
+async def populate_hybrid_collection(hybrid_collection_name: str, stored_points: List[PointStruct], cat: StrayCat):
+    global hybrid_collection_names
 
     points_ids = []
     points_vectors = []
@@ -88,52 +45,109 @@ async def populate_hybrid_collection(stored_points, cat):
         )
         points_payloads.append(p.payload)
 
-    await cat.vector_memory_handler.add_points(
+    await cat.vector_memory_handler.add_points_to_tenant(
         collection_name=hybrid_collection_name, payloads=points_payloads, vectors=points_vectors, ids=points_ids
     )
 
     log.info(f"Added {len(points_ids)} points to hybrid collection")
 
 
-async def search_hybrid_collection(query: str, metadata: Dict, cat):
-    global hybrid_collection_name, k_prefetched, threshold, k
+async def search_hybrid_collection(config: RecallSettings, metadata: Dict, cat) -> List[ScoredPoint]:
+    global hybrid_collection_names, k_prefetched, threshold, k
+
+    finalized_metadata = config.metadata | metadata if config.metadata else metadata
+
     client = cat.vector_memory_handler
-    dense_embedding = cat.embedder.embed_query(query)
-    search_result = await client.search_prefetched(
-        collection_name=hybrid_collection_name,
-        query=query,
-        query_vector=dense_embedding,
-        query_filter=client.filter_from_dict(metadata),
-        k=k,
-        k_prefetched=k_prefetched,
-        threshold=threshold,
-    )
+    search_result = []
+    for hybrid_collection_name in hybrid_collection_names:
+        search_result.extend(await client.search_prefetched_in_tenant(
+            collection_name=hybrid_collection_name,
+            query=cat.working_memory.user_message.text,
+            query_vector=config.embedding,
+            query_filter=client.filter_from_dict(finalized_metadata),
+            k=k,
+            k_prefetched=k_prefetched,
+            threshold=threshold,
+        ))
 
     return search_result
 
 
 @hook(priority=99)
-def before_cat_recalls_memories(
-    declarative_recall_config: RecallSettings, cat
-) -> RecallSettings:
-    global k, threshold
-    declarative_recall_config.k = k
-    declarative_recall_config.threshold = threshold
-    return declarative_recall_config
+def before_cat_reads_message(user_message: UserMessage, cat) -> UserMessage:
+    global k, threshold, k_prefetched
+
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    k = settings["number_of_hybrid_items"]
+    k_prefetched = settings["number_of_prefetched_items"]
+    threshold = settings["hybrid_threshold"]
+
+    return user_message
 
 
 @hook(priority=99)
-def after_cat_recalls_memories(cat) -> None:
+def agent_fast_reply(cat: StrayCat) -> AgenticWorkflowOutput | None:
+    global hybrid_collection_names
+
+    user_message: str = cat.working_memory.user_message.text
+    if not user_message.startswith("@hybrid"):
+        return None
+
+    if user_message == "@hybrid init":
+        for hybrid_collection_name in hybrid_collection_names:
+            run_sync_or_async(delete_hybrid_collection_if_exists, hybrid_collection_name, cat)
+            run_sync_or_async(create_hybrid_collection_if_not_exists, hybrid_collection_name, cat)
+
+        return AgenticWorkflowOutput(output="Hybrid collection initialized.")
+
+    if user_message == "@hybrid migrate":
+        for hybrid_collection_name in hybrid_collection_names:
+            points = run_sync_or_async(get_memory_points, hybrid_collection_name, cat)
+            run_sync_or_async(populate_hybrid_collection, hybrid_collection_name, points, cat)
+
+        # add 5-second wait time to ensure data is committed
+        time.sleep(5)
+
+        return AgenticWorkflowOutput(output="Hybrid collection populted.")
+
+    return None
+
+
+# hybrid collection management
+@hook
+def after_cat_bootstrap(cat: StrayCat):
+    for hybrid_collection_name in hybrid_collection_names:
+        run_sync_or_async(create_hybrid_collection_if_not_exists, hybrid_collection_name, cat)
+
+
+@hook
+def after_rabbithole_stored_documents(source: str, stored_points: List[PointStruct], cat: StrayCat):
+    run_sync_or_async(populate_hybrid_collection, stored_points, cat)
+
+
+@hook(priority=99)
+def before_cat_recalls_memories(recall_config: RecallSettings, cat) -> RecallSettings:
+    global k, threshold
+    recall_config.k = k
+    recall_config.threshold = threshold
+
+    return recall_config
+
+
+@hook(priority=99)
+def after_cat_recalls_memories(config: RecallSettings, cat) -> None:
+    user_message: UserMessage = cat.working_memory.user_message
+
     metadata = {}
     ## if there are tags in the user message, use them as metadata filter
     if (
         hasattr(cat.working_memory.user_message, "tags")
         and cat.working_memory.user_message.tags
     ):
-        metadata = cat.working_memory.user_message.tags
+        metadata = user_message.tags
     memories = run_sync_or_async(
         search_hybrid_collection,
-        cat.working_memory.recall_query,
+        config,
         metadata,
         cat,
     )
@@ -141,14 +155,14 @@ def after_cat_recalls_memories(cat) -> None:
     langchain_documents_from_points = []
     for m in memories:
         langchain_documents_from_points.append(
-            (
-                LangChainDocument(
+            DocumentRecall(
+                document=LangChainDocument(
                     page_content=m.payload.get("page_content"),
                     metadata=m.payload.get("metadata") or {},
                 ),
-                m.score,
-                m.vector,
-                m.id,
+                score=m.score,
+                vector=m.vector,
+                id=m.id,
             )
         )
     cat.working_memory.declarative_memories = langchain_documents_from_points
